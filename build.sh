e16e0bfe958510a90c76087a98fc872f268b144a
SayukiDev <sayukimain@gmail.com>
2024-02-25 20:26:01 +0900

    fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+mkdir -p build
 output="palworld-guard"
 if [ "$GOOS" == "windows" ]
 then
 output="$output.exe"
 fi
-go build -o output -ldflags '-s -w' -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}"
+go build -o "./build/$output" -ldflags '-s -w' -gcflags="all=-trimpath=${PWD}" -asmflags="all=-trimpath=${PWD}"
